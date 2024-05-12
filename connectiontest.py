7c0701c05aba93d34a77e39bd8ff7a1822f6fea6
Saurabh Bhargav <143943258+Saurabh-Bhargav@users.noreply.github.com>
2024-05-12 22:02:32 +0530

    Update connectiontest.py

--- connectiontest.py
+++ connectiontest.py
@@ -1,15 +1,1 @@
-import requests
-
-def test_jenkins_connection(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print("Connection successful! Jenkins is reachable.")
-        else:
-            print(f"Failed to connect to Jenkins. Status code: {response.status_code}")
-    except requests.ConnectionError:
-        print("Failed to connect to Jenkins. Check the URL and try again.")
-
-if __name__ == "__main__":
-    jenkins_url = "http://192.168.233.130:8080" 
-    test_jenkins_connection(jenkins_url)
+print("Hello, world!")
